86db5dfababfae1fd18abc2e6b0a8be8a2aea31b
fkysly <fkysly@hotmail.com>
2023-12-19 20:51:47 +0800

    feat: add fold support

--- plugins.lua
+++ plugins.lua
@@ -88,6 +88,120 @@ local plugins = {
       "nvim-telescope/telescope-fzf-native.nvim",
     },
   },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'kevinhwang91/promise-async',
+       {
+          "luukvbaal/statuscol.nvim",
+          config = function()
+            local builtin = require("statuscol.builtin")
+            require("statuscol").setup(
+              {
+                relculright = true,
+                segments = {
+                  {text = {builtin.foldfunc}, click = "v:lua.ScFa"},
+                  {text = {"%s"}, click = "v:lua.ScSa"},
+                  {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"}
+                }
+              }
+            )
+          end
+
+        }
+    },
+    event = "BufReadPost", -- needed for folds to load in time
+    keys = {
+			{
+				"zr",
+				function() require("ufo").openFoldsExceptKinds { "comment" } end,
+				desc = " 󱃄 Open All Folds except comments",
+			},
+			{ "zm", function() require("ufo").closeAllFolds() end, desc = " 󱃄 Close All Folds" },
+			{
+				"z1",
+				function() require("ufo").closeFoldsWith(1) end,
+				desc = " 󱃄 Close L1 Folds",
+			},
+			{
+				"z2",
+				function() require("ufo").closeFoldsWith(2) end,
+				desc = " 󱃄 Close L2 Folds",
+			},
+			{
+				"z3",
+				function() require("ufo").closeFoldsWith(3) end,
+				desc = " 󱃄 Close L3 Folds",
+			},
+			{
+				"z4",
+				function() require("ufo").closeFoldsWith(4) end,
+				desc = " 󱃄 Close L4 Folds",
+			},
+		},
+		init = function()
+			-- INFO fold commands usually change the foldlevel, which fixes folds, e.g.
+			-- auto-closing them after leaving insert mode, however ufo does not seem to
+			-- have equivalents for zr and zm because there is no saved fold level.
+			-- Consequently, the vim-internal fold levels need to be disabled by setting
+			-- them to 99
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+      vim.o.foldcolumn = '1' -- '0' is not bad
+      vim.o.foldenable = true
+		end,
+		opts = {
+      provider_selector = function()
+        return {'treesitter', 'indent'}
+      end,
+      preview = {
+        win_config = {
+          border = { "", "─", "", "", "", "─", "", "" },
+          -- winhighlight = "Normal:Folded",
+          winblend = 0,
+        },
+        mappings = {
+          scrollU = "<C-u>",
+          scrollD = "<C-d>",
+          jumpTop = "[",
+          jumpBot = "]",
+        },
+      },
+			close_fold_kinds = { "imports", "comment" },
+      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local totalLines = vim.api.nvim_buf_line_count(0)
+        local foldedLines = endLnum - lnum
+        local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        local rAlignAppndx =
+          math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+        suffix = (" "):rep(rAlignAppndx) .. suffix
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
+    },
+  }
 
   -- To make a plugin not be loaded
   -- {
